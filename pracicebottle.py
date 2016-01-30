1e754e4c4ca68f447fc4f4df6608a07640b8373d
Nichalous Gibbons <nick@blue-labs.org>
2016-01-30 03:56:24 +0000

    Insert blackport

--- pracicebottle.py
+++ pracicebottle.py
@@ -1,35 +1,12 @@
 
-##pushing test file to request completion of bottle.py acl conf
-
-"""from bottle import route, run, template
-
-@route('/titania/<name>')
-def index(name):
-	return template('<b> WAKE ME UP {{name}}</b.!', name=name)
-
-	run(host='localhost, port=8080')
-"""
-
-"""##initial pages for simple assignment 1
-from bottle import Bottle, route, run, static_file, abort, error, response
-import subprocess
- 
-app = Bottle()
-
-@app.route('/blackport/<pagename>')
-def blackport():
-	if pagename == route('/blackport/wiekie'):
-		print("hey alex! hi CINDY! was up DJ")
-	elif pagename == route('/blackport/LGT'):
-		print("Was up Light Fam!!!") 
-		print("<img> source = /desktop/buinessss/sunflower.jpg")
-		return
-
-run(app, host='localhost', port=8080, debug=True)"""
 ###################################################################
 
 ##Less than simple assignment 2
 ##make sure no one can get to a nonexisiting extension route of blackport
+
+from bottle import Bottle, route, run, static_file, abort, error, response
+import subprocess
+
 ##add error404.html
 @error(404)
 	def adiquite(response):
