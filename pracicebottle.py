370b85200e36bd4f17e503609b07cbb3f5fd8248
David Ford <david@blue-labs.org>
2016-01-30 04:13:41 +0000

    Re-org some changes for clarity

--- pracicebottle.py
+++ pracicebottle.py
@@ -7,15 +7,30 @@
 from bottle import Bottle, route, run, static_file, abort, error, response
 import subprocess
 
+##add error403.html
+@error(403)
+def ediquite(response):
+	return static_file("error403.html", root = "/blackport)
+
+
 ##add error404.html
 @error(404)
-	def adiquite(response):
+def adiquite(response):
 			return static_file("error404.html", root ="/blackport")
 
 		##include existing possible files so if a case arrises where a file not existing, you get directed to my 404.html
 
+
 @route("/<path:re:.*>", method = "ANY")
-	def furture(path):
+def furture(path):
+		##allow only certain addresses accsess
+
+		if not path in(127.0.0.1, 10.255.0.67, 10.255.0.69, 10.255.0.70., int('')
+			abort(403, "getthouhence!")
+
+		if path == int(''):
+				path = "index2.html"
+
 		if not path in ("", "cube.jpg", "page2.html", "batarang.jpg", "index.html", "sunflower.jpg"):
 				abort(404, "filenotfound")
 
@@ -24,19 +39,6 @@ import subprocess
 
 			return static_file (path, root = "/blackport")
 
-##add error403.html
-@error(403)
-	def ediquite(response):
-			return static_file("error403.html", root = "/blackport)
 
-		##allow only certain addresses accsess
 
-		if not path in(127.0.0.1, 10.255.0.67, 10.255.0.69, 10.255.0.70., int('') 
-			abort(403, "getthouhence!")
-
-		if path == int(''):
-				path = "index2.html"
-
-##run on host 192.168.0.4
-
-run(host = 192.168.0.4, port = 69, debug = True)
+run(host = 10.255.0.67, port = 1069, debug = True)
