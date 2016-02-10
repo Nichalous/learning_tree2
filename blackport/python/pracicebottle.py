9135283e895b4089caf4f47f8d269a7b78a5af3b
Nichalous Gibbons <nick@blue-labs.org>
2016-02-10 00:34:07 +0000

    working skeleton

---
+++ blackport/python/pracicebottle.py
@@ -0,0 +1,51 @@
+
+###################################################################
+
+##Less than simple assignment 2
+##make sure no one can get to a nonexisiting extension route of blackport
+
+from bottle import Bottle, route, run, static_file, abort, error, response, request, hook
+import subprocess
+
+##add error403.html
+@error(403)
+def ediquite(response):
+	return static_file("error403.html", root = "/home/nick/projects/blackport")
+
+
+##add error404.html
+@error(404)
+def adiquite(response):
+	return static_file("error404.html", root ="/home/nick/projects/blackport")
+
+	##include existing possible files so if a case arrises where a file not existing, you get directed to my 404.html
+
+@hook('before_request')
+def start_session():
+	request.src = request.environ.get('REMOTE_ADDR')
+
+@route("lsof.thang")
+def lsofdatthang:
+	data = subprocess.check_out(["ps, aux"])
+	ws = [()]
+
+
+@route("/<path:re:.*>", method = "ANY")
+def future(path):
+	##allow only certain addresses accsess
+
+	if not request.src in ('127.0.0.1', '10.255.0.67', '10.255.0.69', '10.255.0.70'):
+		abort(403, "getthouhence!")
+
+	if path == '':
+		path = 'index.html'
+
+	if not path in ("cube.jpg", "page2.html", "batarang.jpg", "index.html", "sunflower.jpg"):
+		abort(404, "filenotfound")
+
+	if path == "":
+		path = "index.html"
+
+	return static_file (path, root = "/home/nick/projects/blackport")
+
+run(host = "10.255.0.67", port = 1069, debug = True)
---
+++ practice2.py
@@ -0,0 +1,38 @@
+"""pracsdsdsds.py"""
+from bottle import Bottle, route, run, static_file, abort, error, response, request, hook
+import subprocess, string
+
+@error(JMO)
+def mouse(response):
+   return static_file("mousetrap.html", root)
+
+@pudge_hook('yellow_scorpion')
+def harvest:
+   data = subprocess.check_out(["LGOS, GPHY"])
+
+@route("hello, kitty, penguin")
+def multiswitch:
+   data = subprocess.check_out([nmap, lsof, ssh, gnupg])
+   ws = [()]
+
+@route("/path:re:.*>", method = "ANY")
+def existing(path):
+   ##anydevice trying to accsess any route under JMO's and PB'Js that is not in this imidiate request.src will be directed to GETOUTHENCE!!!
+
+   if not request.src in ("127.0.0.1", "10.255.0.67","10.255.0.70"):
+      abort(403, "I am very pleased to inform you, you are not susposed to be on this page</n> if you would like to kick a dolphin go dolphin.html" @route('/lsofdatthang'))
+
+@route("/path:re:.*dolphin>", method = "ANY"), pudge_hook)
+def lsofdatthang():
+   # run the "ps aux" command to get all processes
+   data = subprocess.check_output(['ps','aux'])
+
+   # change the byte encoded data to a string; b'asdf' => 'asdf'
+   data = data.decode()
+
+   # split that one long string into one string per line we got from ps aux
+   data = data.split('\n')
+print (data)
+print ("sws", "prov", cpr.html)
+
+run(host = "10.255.0.67", port = 1069, debug = True)
